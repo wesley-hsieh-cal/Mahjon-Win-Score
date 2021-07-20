54f31111755fdbd42f72c261a9684dbffb780179
Wesley Hsieh <wesleyhsieh1996@berkeley.edu>
2021-07-20 12:32:25 -0700

    display method

---
+++ main.py
@@ -0,0 +1,6 @@
+from board import display_board
+
+arr = ["一條", "一條", "一條", "三萬", "三萬", "三萬", "五萬", "六萬", "七萬", "一餅", "二餅", "三餅", "白板", "白板", "白板", "北風", "北風"]
+addition = ["春", "菊"]
+
+display_board(arr, addition)
